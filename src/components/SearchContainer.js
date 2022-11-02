8b5de6350f957ec826ba97d4c1fdb2ec7b56e1b3
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2022-11-02 23:02:10 +0530

    After Submittion Update )

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -6,10 +6,7 @@ import SharingFooter from "./SharingFooter"
 import JSONDATAPERSON from "../MOCK_DATA_PERSON.json";
 import JSONDATAGROUP from "../MOCK_DATA_GROUP.json";
 
-// img
-import oslash_logo from "../img/oslash_logo.png"
-
-const Widget = () => {
+const SearchContainer = () => {
 
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -44,54 +41,7 @@ const Widget = () => {
     }
 
   return (
-    <section id="widget_wrapper">
-      {!showSearch && <section id="" className="widget_container">
-        {/* Share To Web */}
-        <article className="toggle_section">
-          <div className="toggle_header">
-            <i className="fa-solid fa-earth-americas"></i>
-            <div>
-              <header>Share to web</header>
-              <p>Publish and share link with anyone</p>
-            </div>
-          </div>
-
-          <div>
-            <label className="switch">
-              <input type="checkbox" />
-              <span className="slider round"></span>
-            </label>
-          </div>
-        </article>
-
-        {/* Input */}
-        <article className="input_section">
-          <div className="input_btn_div">
-            {/* <input type="search" placeholder="People, emails, groups" onChange={event => setSearchTerm(event.target.value)} /> */}
-            <div type="search" className="take_to_search" onClick={() => setShowSearch(true)}>People, emails, groups</div>
-            <button className="invite_btn">Invite</button>
-          </div>
-        </article>
-
-        {/* Content Section */}
-        <article className="content_section">
-          <div className="toggle_header">
-            <img src={oslash_logo} alt="avatar" className="avatar" />
-            <div>
-              <header>Everyone at OSlash</header>
-              <p>25 workspace members</p>
-            </div>
-          </div>
-
-          <Dropdown />
-        </article>
-        {personToWidget}
-
-        {/* Sharing Footer */}
-        <SharingFooter />
-      </section>}
-
-      {showSearch && <section id="search_window_container" className="">
+    <section id="search_window_container" className="">
         {/* Search Window Input */}
         <article className="search_input_section">
           <div className="search_input_btn_div">
@@ -147,10 +97,8 @@ const Widget = () => {
 
         {/* Footer  */}
         <SharingFooter />
-      </section>}
-
-    </section>
+      </section>
   )
 };
 
-export default Widget;
+export default SearchContainer;
